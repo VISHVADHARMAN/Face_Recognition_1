6535b78fcd736ab3e459744749d05f49ad817945
VISHVADHARMAN <67373113+VISHVADHARMAN@users.noreply.github.com>
2023-11-30 18:01:57 +0400

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,24 @@
+import Program
+import time
+
+def initial():
+    time.sleep(2)
+    print("\n####\nFor Registering face, Enter 1  \nFor Attendance, Enter 2 \nFor Exit, Enter 0\n####\n")
+    a1=input('Give your input : ')
+    print()
+    if a1=='1':
+        if Program.Input():
+            return initial()
+    elif a1=='2':
+        if Program.face_rec():
+            return initial()
+    elif a1=='0':
+        print("Thank You! See you Soon...\n")
+        exit
+    else:
+        print("#### Please Give The Correct Value ####")
+        return initial()
+
+print("\n  \"Introduction to Engineering Project\"")
+print("---Facial Recognition Attendance System---")
+initial()
